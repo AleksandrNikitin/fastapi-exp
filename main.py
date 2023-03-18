994a9d2d5d3a2d48e82574eb510820e872e18ff3
02_nikitin_aa <a.nikit.in@mail.ru>
2023-03-18 04:14:26 +0300

    ver2

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+# Message class defined in Pydantic
+class Message(BaseModel):
+    channel: str
+    author: str
+    text: str
+
 
 app = FastAPI()
 
 @app.get("/show")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/post_message")
+def post_message(message: Message):
+    return message
