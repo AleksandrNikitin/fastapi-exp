30477b9f3e129acc68a214816d8c1cef22a022b1
02_nikitin_aa <a.nikit.in@mail.ru>
2023-03-18 05:59:16 +0300

    v3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import requests
+import secret
 
 # Message class defined in Pydantic
 class Task(BaseModel):
@@ -17,8 +18,20 @@ class Task(BaseModel):
 
 app = FastAPI()
 
+@app.get("/")
+def status():
+    return {"status":"up"}
 
 @app.post("/push_task", status_code=201)
 def post_message(task: Task):
+
+    task_param = task.dict()
+    chat_id = task_param.chat_id
+    message = task_param.key
+
+    url = f"https://api.telegram.org/bot{secret.TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+
+    result = requests.get(url)
+
 
     return {"result":"1"}
