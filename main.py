da9fdc30ffcee764d7ddc0b137ded3639a9e2b81
02_nikitin_aa <a.nikit.in@mail.ru>
2023-03-19 12:40:23 +0300

    release1.0

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 import secret
+from emoji import emojize
 
-# Message class defined in Pydantic
+
 class Task(BaseModel):
     chat_id: str
     key: str
@@ -13,32 +14,48 @@ class Task(BaseModel):
     type: str
     status: str
     created: str
+    creator: str
 
-
+class Alert(BaseModel):
+    chat_id: str
+    task_key: str
+    task_sum: str
+    message: str
 
 app = FastAPI()
 
 @app.get("/")
-def status():
+async def get_status():
     return {"status":"up"}
 
+
 @app.post("/push_task", status_code=201)
-def post_message(task: Task):
-
+async def push_task(task: Task):
     task_param = task.dict()
-    chat_id = task_param['chat_id']
-    key = task_param['key']
-    sum = task_param['sum']
-    priority = task_param['priority']
-    type = task_param['type']
-    status = task_param['status']
-    created = task_param['created']
 
-    message = f"Новый {type} c {priority} приоритетом!!!\n\nhttps://jira.app.local/browse/{key}\n\n{sum}\n\n{created}"
+    message = f"{emojize(':red_circle:', language='alias')}Внимание! Новый {task_param['type']}!\n" \
+              f"{emojize(':bangbang:', language='alias')}{task_param['priority']} приоритет\n\n" \
+              f"https://jira.app.local/browse/{task_param['key']}\n\n" \
+              f"{task_param['sum']}\n\n" \
+              f"{task_param['descr']}\n\n" \
+              f"Дата создания: {task_param['created']}\n" \
+              f"Автор: {task_param['creator']}"
 
-    url = f"https://api.telegram.org/bot{secret.TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-
+    url = f"https://api.telegram.org/bot{secret.TOKEN}/sendMessage?chat_id={task_param['chat_id']}&text={message}"
     result = requests.get(url)
+    return {"result":"1"}
 
 
+@app.post("/push_alert", status_code=201)
+async def push_alert(alert: Alert):
+    alert_param = alert.dict()
+
+    message = f"{emojize(':red_circle:', language='alias')}Внимание!\n"\
+              f"{emojize(':alarm_clock:', language='alias')}{alert_param['message']}\n\n" \
+              f"https://jira.app.local/browse/{alert_param['task_key']}\n" \
+              f"{alert_param['task_sum']}\n" \
+
+
+    url = f"https://api.telegram.org/bot{secret.TOKEN}/sendMessage?chat_id={alert_param['chat_id']}&text={message}"
+    result = requests.get(url)
     return {"result":"1"}
